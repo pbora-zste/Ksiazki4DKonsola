ede493e11b923733b4b94791f91c484750abfe74
pbora-zste <9462502+pbora-zste@users.noreply.github.com>
2026-02-02 12:02:58 +0100

    Implementacja klasy Ksiazka

---
+++ Ksiazki4DKonsola/Ksiazka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksiazki4DKonsola
+{
+    internal class Ksiazka
+    {
+        public List<Strona> strony = new List<Strona>();
+        public Ksiazka(string tytul)
+        {
+            var strona = new Strona(tytul, 1);
+            strony.Add(strona);
+        }
+        public void DodajStrone(string trescStrony)
+        {
+            var strona = new Strona(trescStrony);
+            strony.Add(strona);
+        }
+    }
+}
